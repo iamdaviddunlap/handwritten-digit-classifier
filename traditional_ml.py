c376b119ada34a2a84fa4593416e3949ac4c1d98
David <dad9489@rit.edu>
2022-04-12 00:24:06 -0400

    Added debug output to print how long prediction takes

--- traditional_ml.py
+++ traditional_ml.py
@@ -3,14 +3,15 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.utils import shuffle
 from sklearn.metrics import confusion_matrix
+from time import time
 
 from load_data import load_mnist, load_kannada
 
-mnist = load_mnist()
-kannada = load_kannada()
+mnist_dataset = load_mnist()
+kannada_dataset = load_kannada()
 
-(total_x_train, total_y_train), (total_x_test, total_y_test) = mnist.get_data()
-for (x_train, y_train), (x_test, y_test) in [kannada.get_data()]:
+(total_x_train, total_y_train), (total_x_test, total_y_test) = mnist_dataset.get_data()
+for (x_train, y_train), (x_test, y_test) in [kannada_dataset.get_data()]:
     total_x_train = np.concatenate((total_x_train, x_train))
     total_y_train = np.concatenate((total_y_train, y_train))
     total_x_test = np.concatenate((total_x_test, x_test))
@@ -20,8 +21,11 @@ total_x_train, total_y_train = shuffle(total_x_train, total_y_train)
 total_x_test, total_y_test = shuffle(total_x_test, total_y_test)
 
 clf = MultiOutputClassifier(KNeighborsClassifier()).fit(total_x_train, total_y_train)
-y_pred = clf.predict(total_x_test[:1000])
-y_true = total_y_test[:1000]
+print('Starting prediction')
+start = time()
+y_pred = clf.predict(total_x_test)
+print(f'Finished prediction in {time() - start}s')
+y_true = total_y_test
 
 print('Language')
 print(confusion_matrix(y_true[:, 0], y_pred[:, 0]))
